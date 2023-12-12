b93ac13ed5bb77eff472167cd58fd4a5d4774410
Nakamoto-Kouta-Fukuyama <21013112@anabuki-net.ne.jp>
2023-12-12 12:05:23 +0900

    00_34

--- 3DLv1_vs2019_00/GameProgramming/src/CCollider.h
+++ 3DLv1_vs2019_00/GameProgramming/src/CCollider.h
@@ -13,6 +13,11 @@ class CCollisionManager;
 class CCollider :public CTransform,public CTask {
 	friend CCollisionManager;
 public:
+	//CollisionTriangleSphere(三角コライダ,球コライダ,調整値)
+	//retrun:true(衝突している)false(衝突していない)
+	//調整値:衝突しない位置まで戻す値
+	static bool CollisionTriangleSphere(CCollider* triangle, CCollider* sphere, CVector* adjust);
+
 	//Collision TriangleLine(三角コライダ,線分コライダ,調整値)
 	//return:true(衝突している)false(衝突していない)
 	//調整値:衝突しない位置まで戻す値
