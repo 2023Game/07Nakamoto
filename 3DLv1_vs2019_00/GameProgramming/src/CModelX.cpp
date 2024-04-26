afc82a437de85a9534f229a62acef47e6f80983c
Nakamoto-Kouta-Fukuyama <21013112@anabuki-net.ne.jp>
2024-04-26 10:36:32 +0900

    00_07

--- 3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
@@ -8,15 +8,18 @@
 //コンストラクタ
 CMesh::CMesh()
 	:mVertexNum(0)
-	,mpVertex(nullptr)
-	,mFaceNum(0)
-	,mpVertexIndex(nullptr)
+	, mpVertex(nullptr)
+	, mFaceNum(0)
+	, mpVertexIndex(nullptr)
+	, mNormalNum(0)
+	, mpNormal(nullptr)
 {}
 
 //デストラクタ
 CMesh::~CMesh() {
 	SAFE_DELETE_ARRAY(mpVertex);
 	SAFE_DELETE_ARRAY(mpVertexIndex);
+	SAFE_DELETE_ARRAY(mpNormal);
 }
 
 /*
@@ -52,6 +55,38 @@ void CMesh::Init(CModelX* model) {
 		mpVertexIndex[i + 2] = atoi(model->GetToken());
 	}
 
+	model->GetToken();	//MeshNormals
+	if (strcmp(model->Token(), "MeshNormals") == 0) {
+		model->GetToken();	// {
+		//法線データを取得
+		mNormalNum = atoi(model->GetToken());
+		//法線のデータを配列に取り込む
+		CVector* pNormal = new CVector[mNormalNum];
+		for (int i = 0; i < mNormalNum; i++) {
+			pNormal[i].X(atof(model->GetToken()));
+			pNormal[i].Y(atof(model->GetToken()));
+			pNormal[i].Z(atof(model->GetToken()));
+		}
+		//法線数 = 面数 × 3
+		mNormalNum = atoi(model->GetToken()) * 3;	//FaceNum
+		int ni;
+		//頂点毎に法線データを設定する
+		mpNormal = new CVector[mNormalNum];
+		for (int i = 0; i < mNormalNum; i += 3) {
+			model->GetToken();	// 3
+			ni = atoi(model->GetToken());
+			mpNormal[i] = pNormal[ni];
+
+			ni = atoi(model->GetToken());
+			mpNormal[i + 1] = pNormal[ni];
+
+			ni = atoi(model->GetToken());
+			mpNormal[i + 2] = pNormal[ni];
+		}
+		delete[] pNormal;
+		model->GetToken();	// }
+	}	// End of MesfNormals
+
 	//デバッグバージョンのみ有効
 #ifdef _DEBUG
 	printf("VertexNum:%d\n",mVertexNum);
@@ -65,6 +100,12 @@ void CMesh::Init(CModelX* model) {
 	for (int i = 0; i < mFaceNum * 3; i += 3) {
 		printf("%2d %2d %2d\n",
 			mpVertexIndex[i], mpVertexIndex[i + 1], mpVertexIndex[i + 2]);
+	}
+
+	printf("NormalNum:%d\n", mNormalNum);
+	for (int i = 0; i < mNormalNum; i++) {
+		printf("%10f %10f %10f\n",
+			mpNormal[i].X(), mpNormal[i].Y(), mpNormal[i].Z());
 	}
 #endif
 
