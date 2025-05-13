42ecb5cbfed3f0fdf0a2f2e65b0328287587767b
Nakamoto-Kouta-Fukuyama <21013112@anabuki-net.ne.jp>
2025-05-13 15:26:34 +0900

    Add

---
+++ 魂結びの廃墟/BaseProject/BaseProject/BaseProject/src/Game/CField.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "CObjectBase.h"
+#include "CColliderMesh.h"
+
+class CField : public CObjectBase
+{
+public:
+	CField();
+	~CField();
+
+	void Update();
+	void Render();
+
+private:
+	void CreateFieldObjects();
+
+	CModel* mpModel;
+	CModel* mpModel2;
+	CColliderMesh* mpColliderMesh;
+
+	CModel* mpCubeModel;
+	CModel* mpCylinderModel;
+
+	TexAnimData mEffectAnimData;
+};
